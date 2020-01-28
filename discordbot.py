40604cd4cc508b425728194d10d7286fd46d3b98
MaidenSyn <47996569+MaidenSyn@users.noreply.github.com>
2020-01-28 03:07:16 -0600

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['BOT_TOKEN']
@@ -12,10 +13,14 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+names = ['Sam', 'Paul', 'Mark', 'Simon', 'Sean', 'Samantha', 'Ellen']
+random_name = random.choice(names)
+
+print(random_name)
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send(random_name)
     
 
 
