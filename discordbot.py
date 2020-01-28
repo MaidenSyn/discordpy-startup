3d9dcaf8afce18394303d7ae746ffb200b4a6eb0
MaidenSyn <47996569+MaidenSyn@users.noreply.github.com>
2020-01-28 02:42:48 -0600

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,19 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+    
+    
+@bot.event
+async def on_member_join(member):
+    print(f'(member) has joined a server.')
+    
+    
+    
+
+@bot.event
+async def on_member_remove(member):
+    print(f'(member) has left a server.')
 
 
 bot.run(token)
